97cb0d9c70e58fe04f704094ec6881464923efd0
Guillaume377 <guillaumestephan@live.fr>
2023-10-02 16:56:18 +0200

    correction d'erreur

--- src/components/FrenchMovies.vue
+++ src/components/FrenchMovies.vue
@@ -1,12 +1,12 @@
 <template>
 
-            <!-- j'affiche le titre de la page et sa sélection de films (films américains)-->
+            <!-- j'affiche le titre de la page et sa sélection de films (films français)-->
             <h1 class="pt-5 m-5 text-light font-weight-light">
                 Vos films français sont sur <span class = "text-danger">Cinéflix</span> !
             </h1>
 
                 <SortButtons :movies="frenchMovies"></SortButtons>
-                <!-- v-bind : prop attendue = variable des dates (liste de films)-->
+                <!-- v-bind : prop attendue = variable des datas (liste de films)-->
                 <MoviesList v-bind:movies="frenchMovies" />
 
 </template>
@@ -14,7 +14,6 @@
 <script>
 // import d'axios pour pouvoir faire des appels API
 import axios from "axios"
-
 import MoviesList from './utils/MoviesList.vue';
 import SortButtons from "./utils/SortButtons.vue";
 
@@ -36,8 +35,10 @@ export default {
     created() {
         //code déclenché avant la génération du template par vue
         //c'est ici que je vais lancer mon appel API
-        // adresse de l'API     / version / mot-clé / clé API                                / options : français + popularité description. + page 1
-        axios.get("https://api.themoviedb.org/3/discover/movie/?api_key=a5087ee297fbc59075d15615744b267d&with_original_language=fr&language=fr&include_adult=false&vote_count.gte=1000&page=1")
+        // adresse de l'API     / version / mot-clé / clé API / options : film tourné en français + infos récupérés en français 
+        //+ films tous publics + de 1000 votes + 1ère page des résultats
+        axios.get("https://api.themoviedb.org/3/discover/movie/?api_key=a5087ee297fbc59075d15615744b267d&with_original_language=fr"
+                +"&language=fr&include_adult=false&vote_count.gte=1000&page=1")
             // .then => cas où l'appel API a réussi et renvoie un résultat
             // .then => prend en paramètre une fonction fléchée anonyme. res= réponse de l'API
             .then(res => {
@@ -47,7 +48,6 @@ export default {
             // .catch => cas où l'appel API échoue / à faire pour chaque API
             .catch(() => this.error = true)
     }
-
 }
 </script>
 
